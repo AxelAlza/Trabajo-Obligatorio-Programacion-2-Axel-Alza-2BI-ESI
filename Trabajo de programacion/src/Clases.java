1f9f7ce46e0b8b7e0a63f2251bfa51fa02a5e21c
AxelAlza <axelalza1998@gmail.com>
2020-04-01 00:09:59 -0300

    De una

---
+++ Trabajo de programacion/src/Clases.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+
+class Clases {
+
+	ArrayList<Empleados> Emps = new ArrayList<Empleados>();
+
+	// Seguramente aca haya una forma mas organizada y menos rebuscada de hacerlo,
+	// pero queria probar como funciona la "herencia" de los objetos......
+	
+	// Me parece que no entendi nada de que va la idea de la herencia por que me
+	// esta creando mas problemas de los que soluciona profe help jaja
+
+	public class Empleados {
+		long Cedula;
+		String Nombre;
+		String Apellido;
+		String Direccion;
+		long Telefono[];
+		double Saldo = 15000;
+		String Tipo_Empleado = "Común";
+
+		public Empleados(long Cedula, String Nombre, String Apellido, String Direccion, long Telefono[]) {
+			this.Cedula = Cedula;
+			this.Nombre = Nombre;
+			this.Apellido = Apellido;
+			this.Direccion = Direccion;
+			this.Telefono = Telefono;
+		}
+
+	}
+
+	public class Gerente extends Empleados {
+		double Saldo = super.Saldo + super.Saldo * 0.97;
+		String Tipo_Empleado = "Gerente";
+
+		public Gerente(long Cedula, String Nombre, String Apellido, String Direccion, long[] Telefono) {
+			super(Cedula, Nombre, Apellido, Direccion, Telefono);
+			super.Saldo = this.Saldo;
+			super.Tipo_Empleado = this.Tipo_Empleado;
+		}
+
+	}
+
+	public class Operario extends Empleados {
+		double Saldo = super.Saldo + super.Saldo * 0.38;
+		String Tipo_Empleado = "Operario";
+
+		public Operario(long Cedula, String Nombre, String Apellido, String Direccion, long[] Telefono) {
+			super(Cedula, Nombre, Apellido, Direccion, Telefono);
+			super.Saldo = this.Saldo;
+			super.Tipo_Empleado = this.Tipo_Empleado;
+		}
+
+	}
+
+	public class Administrativo extends Empleados {
+		double Saldo = super.Saldo + super.Saldo * 0.27;
+		String Tipo_Empleado = "Admistrativo";
+
+		public Administrativo(long Cedula, String Nombre, String Apellido, String Direccion, long[] Telefono) {
+			super(Cedula, Nombre, Apellido, Direccion, Telefono);
+			super.Saldo = this.Saldo;
+			super.Tipo_Empleado = this.Tipo_Empleado;
+
+		}
+
+	}
+
+	public void Addemp(long Cedula, String Nombre, String Apellido, String Direccion, long[] Telefono, byte tipo) {
+		switch (tipo) {
+		case 1:
+			Emps.add(new Gerente(Cedula, Nombre, Apellido, Direccion, Telefono));
+			break;
+		case 2:
+			Emps.add(new Operario(Cedula, Nombre, Apellido, Direccion, Telefono));
+			break;
+		case 3:
+			Emps.add(new Administrativo(Cedula, Nombre, Apellido, Direccion, Telefono));
+			break;
+
+		default:
+			System.out.println("Seleccione un tipo de la lista");
+			break;
+		}
+	}
+
+	public void calcularsaldos() {
+		double Saldo_total = 0;
+		for (Empleados empleado : Emps) {
+			System.out.println(empleado.Tipo_Empleado + " " + empleado.Nombre + " " + empleado.Apellido + " -" + " $"
+					+ empleado.Saldo);
+			Saldo_total = Saldo_total + empleado.Saldo;
+		}
+		System.out.println("TOTAL - $" + Saldo_total);
+
+	}
+}
---
+++ Trabajo de programacion/src/Interfaz.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+
+
+public class Interfaz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte tipo = 0;
+		byte op2;
+		long Cedula;
+		String Nombre;
+		String Apellido;
+		String Direccion;
+		long[] Telefono = new long[5];		
+		Clases Clases = new Clases();
+		Scanner dato = new Scanner(System.in);
+		System.out.println("Bienvenido");
+		byte op;
+		do {
+			
+			System.out.println("1- Registro de empleados");
+			System.out.println("2 - Calculo total de saldos");
+			System.out.println("3 - Salir");
+			System.out.println("Seleccione una opción");
+			op = dato.nextByte();
+			switch (op) {
+			case 1:
+				byte i = 0;
+				System.out.println("Registro de empleados");
+				System.out.println("Ingrese Cedula");
+				Cedula = dato.nextLong();
+				System.out.println("Ingrese Nombre");
+				Nombre = dato.next();
+				System.out.println("Ingrese Apellido");
+				Apellido = dato.next();
+				System.out.println("Ingrese Direccion");
+				Direccion = dato.next();
+				System.out.println("Ingrese Telefonos");	
+				do {			
+					Telefono[i] = dato.nextLong();
+					System.out.println("Para dejar de agregar presione 0");
+					op2 = dato.nextByte();
+					i++;
+				}
+				while (op2 !=0);	
+				System.out.println("Ingrese tipo de empleado:"+"\n");
+				System.out.println("1-Gerente");
+				System.out.println("2-Operario");
+				System.out.println("3-Administrativo");
+				op2 = dato.nextByte();
+				switch(op2) {
+				case 1: tipo = 1; break;
+				case 2: tipo = 2; break;
+				case 3: tipo = 3; break;
+				default: System.out.println("Eliga algo de la lista"); break;
+				}
+			
+				Clases.Addemp(Cedula, Nombre, Apellido, Direccion, Telefono, tipo);
+				break;
+			case 2:
+				Clases.calcularsaldos();
+				break;
+			case 3:
+				System.out.println("Salir");
+				break;
+			}
+		} while (op != 3);
+		dato.close();
+	}
+}
